c1a8e67fc045fc4c4d72408bc53b74c622bf9ea3
jupegarnica <jupegarnica@gmail.com>
2019-05-21 13:32:50 +0200

    category logic done

--- src/app/movies-category/movies-category.component.ts
+++ src/app/movies-category/movies-category.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies-category',
   templateUrl: './movies-category.component.html',
-  styleUrls: ['./movies-category.component.scss']
+  styleUrls: ['./movies-category.component.scss'],
 })
 export class MoviesCategoryComponent implements OnInit {
+  category: string;
+  validCategories = ['top_rated', 'upcoming', 'popular'];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-  }
+    this.route.params.subscribe(params => {
+      this.category = params.category.replace('_', ' ');
 
+      if (this.validCategories.includes(params.category)) {
+        // get movies
+      } else {
+        // redirect to /movies/popular
+        this.router.navigate(['/movies/popular'])
+      }
+    });
+  }
 }
